20dba5f0ed7c95c91bb7cd72cddc1f1a8a621b72
desarrolladorns427 <nash6907@gmail.com>
2024-09-01 22:31:59 -0500

    Laboratorio AtoScaling EC2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola Campistas');
+  res.end(`Hola Campistas el Servidor esta Escuchando por el Puerto: ${port}`);
 });
 
 server.listen(port, () => {
